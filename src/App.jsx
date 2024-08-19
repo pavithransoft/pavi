b4ae26d0f0e95be8ccd0cd78a4b2e3d59e20d57f
Pavithran Gopalakrishnan <pavithran.soft@gmail.com>
2024-08-19 16:42:44 +0530

    profile commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-// import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Projects from "./pages/Projects";
 import Blog from "./pages/Blog";
-// import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -13,11 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="about" element={<About />} /> */}
           <Route path="skills" element={<Skills />} />
           <Route path="projects" element={<Projects />} />
           <Route path="blog" element={<Blog />} />
-          {/* <Route path="contact" element={<Contact />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/About.jsx
+++
@@ -1,5 +0,0 @@
-const About = () => {
-  return <div className="bg-slate-100 py-24">About</div>;
-};
-
-export default About;
