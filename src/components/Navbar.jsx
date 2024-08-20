2abeacb2b3d5ba70334623969fe91353b53a8004
Pavithran Gopalakrishnan <pavithran.soft@gmail.com>
2024-08-20 08:08:18 +0530

    profile commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,105 +1,13 @@
-// import { Link, NavLink } from "react-router-dom";
-// import { FaLinkedinIn, FaGithub } from "react-icons/fa";
-// import { assets } from "../assets/assets";
-// import { useEffect, useState } from "react";
-
-// const menus = [
-//   {
-//     path: "/",
-//     name: "HOME",
-//   },
-//   {
-//     path: "/skills",
-//     name: "SKILLS",
-//   },
-//   {
-//     path: "/projects",
-//     name: "PROJECTS",
-//   },
-//   {
-//     path: "/blog",
-//     name: "BLOG",
-//   },
-// ];
-
-// const Navbar = () => {
-//   const [scrolled, setScrolled] = useState(false);
-
-//   useEffect(() => {
-//     const handleScroll = () => setScrolled(window.scrollY > 100);
-//     window.addEventListener("scroll", handleScroll);
-//     return () => window.removeEventListener("scroll", handleScroll);
-//   }, []);
-
-//   return (
-//     <nav
-//       className={`fixed top-0 transition-all duration-300 ease-in-out z-40 bg-slate-900 text-slate-50 grid grid-cols-3 place-items-center w-full ${
-//         scrolled ? "py-1 2xl:py-3 bg-opacity-60" : "py-3 2xl:py-7"
-//       }`}
-//     >
-//       <Link to="/">
-//         <div>
-//           <img
-//             src={assets.d}
-//             alt="Developer"
-//             className="h-6 w-6 2xl:w-9 2xl:h-9 cursor-pointer"
-//           />
-//           <img
-//             src={assets.p}
-//             alt="Pavithran"
-//             className="h-6 w-6 2xl:w-9 2xl:h-9 cursor-pointer"
-//           />
-//         </div>
-//       </Link>
-//       <div className="flex items-center gap-10 text-sm">
-//         {menus.map((menu, index) => (
-//           <NavLink
-//             key={index}
-//             to={menu.path}
-//             className={({ isActive }) =>
-//               `select-none p-2 duration-300 border-b-2 2xl:border-b-4 2xl:text-2xl ${
-//                 isActive
-//                   ? "font-bold text-slate-300 border-green-500"
-//                   : "font-light border-transparent hover:border-red-500"
-//               }`
-//             }
-//           >
-//             {menu.name}
-//           </NavLink>
-//         ))}
-//       </div>
-//       <div className="grid grid-cols-2 gap-5">
-//         <FaLinkedinIn className="h-5 w-5 2xl:h-8 2xl:w-8" />
-//         <FaGithub className="h-5 w-5 2xl:h-8 2xl:w-8" />
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import { Link, NavLink } from "react-router-dom";
 import { FaLinkedinIn, FaGithub, FaBars } from "react-icons/fa";
 import { assets } from "../assets/assets";
 import { useEffect, useState } from "react";
 
 const menus = [
-  {
-    path: "/",
-    name: "HOME",
-  },
-  {
-    path: "/skills",
-    name: "SKILLS",
-  },
-  {
-    path: "/projects",
-    name: "PROJECTS",
-  },
-  {
-    path: "/blog",
-    name: "BLOG",
-  },
+  { path: "/", name: "HOME" },
+  { path: "/skills", name: "SKILLS" },
+  { path: "/projects", name: "PROJECTS" },
+  { path: "/blog", name: "BLOG" },
 ];
 
 const Navbar = () => {
@@ -111,6 +19,20 @@ const Navbar = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  useEffect(() => {
+    // Disable scroll when sidebar is open
+    if (isSidebarOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Cleanup function to reset overflow when component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isSidebarOpen]);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -215,7 +137,7 @@ const Navbar = () => {
             </NavLink>
           ))}
         </div>
-        <div className="flex justify-around p-4 border-t border-slate-700">
+        <div className="flex justify-center items-center gap-10 p-10 border-t border-slate-700">
           <FaLinkedinIn className="h-6 w-6 cursor-pointer" />
           <FaGithub className="h-6 w-6 cursor-pointer" />
         </div>
