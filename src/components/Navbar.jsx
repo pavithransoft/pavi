923b3d71f8aba078dfb3046af2d4ce117bf154a5
Pavithran Gopalakrishnan <pavithran.soft@gmail.com>
2024-08-20 07:53:47 +0530

    profile commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -118,12 +118,12 @@ const Navbar = () => {
 
   return (
     <>
-      <nav
-        className={`fixed top-0 transition-all duration-300 ease-in-out z-40 bg-slate-900 text-slate-50 grid grid-cols-2 items-center w-full ${
-          scrolled ? "py-1 2xl:py-3 bg-opacity-60" : "py-3 2xl:py-7"
-        }`}
-      >
-        {!isSidebarOpen && (
+      {!isSidebarOpen && (
+        <nav
+          className={`fixed top-0 transition-all duration-300 ease-in-out z-40 bg-slate-900 text-slate-50 grid grid-cols-2 items-center w-full ${
+            scrolled ? "py-1 2xl:py-3 bg-opacity-60" : "py-3 2xl:py-7"
+          }`}
+        >
           <Link to="/">
             <div className="px-5">
               <img
@@ -138,33 +138,36 @@ const Navbar = () => {
               />
             </div>
           </Link>
-        )}
 
-        <div className="hidden md:flex items-center gap-10 text-sm">
-          {menus.map((menu, index) => (
-            <NavLink
-              key={index}
-              to={menu.path}
-              className={({ isActive }) =>
-                `select-none p-2 duration-300 border-b-2 2xl:border-b-4 2xl:text-2xl ${
-                  isActive
-                    ? "font-bold text-slate-300 border-green-500"
-                    : "font-light border-transparent hover:border-red-500"
-                }`
-              }
-            >
-              {menu.name}
-            </NavLink>
-          ))}
-        </div>
-        <div className="hidden md:grid grid-cols-2 gap-5">
-          <FaLinkedinIn className="h-5 w-5 2xl:h-8 2xl:w-8" />
-          <FaGithub className="h-5 w-5 2xl:h-8 2xl:w-8" />
-        </div>
-        <div className="md:hidden flex justify-end w-full px-4">
-          <FaBars className="h-6 w-6 cursor-pointer" onClick={toggleSidebar} />
-        </div>
-      </nav>
+          <div className="hidden md:flex items-center gap-10 text-sm">
+            {menus.map((menu, index) => (
+              <NavLink
+                key={index}
+                to={menu.path}
+                className={({ isActive }) =>
+                  `select-none p-2 duration-300 border-b-2 2xl:border-b-4 2xl:text-2xl ${
+                    isActive
+                      ? "font-bold text-slate-300 border-green-500"
+                      : "font-light border-transparent hover:border-red-500"
+                  }`
+                }
+              >
+                {menu.name}
+              </NavLink>
+            ))}
+          </div>
+          <div className="hidden md:grid grid-cols-2 gap-5">
+            <FaLinkedinIn className="h-5 w-5 2xl:h-8 2xl:w-8" />
+            <FaGithub className="h-5 w-5 2xl:h-8 2xl:w-8" />
+          </div>
+          <div className="md:hidden flex justify-end w-full px-4">
+            <FaBars
+              className="h-6 w-6 cursor-pointer"
+              onClick={toggleSidebar}
+            />
+          </div>
+        </nav>
+      )}
 
       {/* Sidebar */}
       <div
