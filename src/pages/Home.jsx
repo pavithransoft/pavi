208fd578525bfc6b41fb8d81748b4628b920ed25
Pavithran Gopalakrishnan <pavithran.soft@gmail.com>
2024-08-21 08:03:40 +0530

    updated commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import { assets } from "./../assets/assets";
 
 const Home = () => {
   return (
-    <section className="bg-white text-slate-900 pb-20 2xl:pb-32">
+    <section className="bg-white text-slate-900 py-16">
       <div className="relative">
-        <p className="absolute grid place-items-center inset-0 z-1 text-3xl 2xl:text-5xl text-center text-white bg-white bg-opacity-15 font-dancing">
+        <p className="absolute grid place-items-center inset-0 z-1 text-xs text-center text-white bg-white bg-opacity-15 font-mono px-1">
           I enjoy coding things from scratch and bringing ideas to life as
           interactive experiences in the browser.
         </p>
@@ -15,26 +15,26 @@ const Home = () => {
           className="w-full h-full object-cover z-0"
         />
       </div>
-      <div className="grid grid-cols-2 place-items-center pt-10 pb-20 2xl:py-32">
-        <div className="w-4/5 m-auto">
+      <div className="grid sm:grid-cols-2 gap-10 place-items-center py-16 px-3">
+        <div className="sm:w-4/5 m-auto order-2 sm:order-1">
           <img src={assets.developer} alt="Developer" />
         </div>
-        <div className="w-4/5 m-auto">
-          <h2 className="text-center text-3xl 2xl:text-6xl font-mono py-5 text-slate-950">
+        <div className="sm:w-4/5 m-auto order-1 sm:order-2">
+          <h2 className="text-center text-2xl font-mono py-5 text-slate-950">
             Who am I?
           </h2>
-          <p className="text-center font-dancing text-2xl 2xl:text-5xl text-slate-600 2xl:leading-loose">
+          <p className="text-center text-xl text-slate-600 leading-relaxed">
             I am a creative thinker, designer, and developer who enriches brands
             and delivers comprehensive technology services that meet the full
             spectrum of business needs.
           </p>
         </div>
       </div>
-      <div className="bg-slate-950 py-14 2xl:py-32">
-        <h2 className="text-2xl 2xl:text-5xl text-slate-50 text-center pb-5 2xl:pb-8 font-dancing">
+      <div className="bg-slate-950 py-8">
+        <h2 className="text-2xl text-slate-50 text-center pb-8 border-b">
           About me
         </h2>
-        <p className="px-48 text-center text-lg 2xl:text-4xl text-slate-400 font-mono 2xl:leading-relaxed">
+        <p className="px-3 pt-8 sm:px-48 text-center text-slate-400 font-mono leading-loose">
           I am a passionate, friendly, and dedicated Frontend Developer who
           thrives on challenging my skills as both a UI designer and developer.
           My work is not just my profession, but also my hobby. This passion
@@ -44,22 +44,22 @@ const Home = () => {
           collaborator.
         </p>
       </div>
-      <div className="w-3/4 m-auto 2xl:py-10">
-        <h2 className="text-center text-2xl 2xl:text-5xl font-mono text-slate-950 py-20 2xl:py-32">
+      <div className="w-3/4 m-auto">
+        <h2 className="text-center text-xl sm:text-2xl font-mono text-slate-950 py-16">
           My Services
         </h2>
-        <div className="grid grid-cols-3 place-items-center gap-10 2xl:gap-20">
+        <div className="grid sm:grid-cols-3 place-items-center gap-8">
           {services.map((service, index) => (
             <div
               key={index}
-              className="grid place-items-center gap-5 bg-slate-100 hover:bg-slate-200 w-full py-5 2xl:py-10 rounded-lg border"
+              className="grid place-items-center gap-4 bg-slate-100 hover:bg-slate-200 w-full py-4 rounded-lg border"
             >
               <img
                 src={service.icon}
                 alt={service.title}
-                className="h-12 w-12 2xl:h-24 2xl:w-24"
+                className="h-10 w-10 sm:h-16 sm:w-16"
               />
-              <p className="font-medium text-slate-700 2xl:text-4xl 2xl:pt-5">
+              <p className="font-medium text-slate-700 text-sm">
                 {service.title}
               </p>
             </div>
