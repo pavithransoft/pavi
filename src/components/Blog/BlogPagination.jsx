664b1be38dff53a49671c55965affb6c926ed3c7
Pavithran Gopalakrishnan <pavithran.soft@gmail.com>
2024-08-21 12:39:07 +0530

    updated commit

--- src/components/Blog/BlogPagination.jsx
+++ src/components/Blog/BlogPagination.jsx
@@ -7,21 +7,21 @@ const BlogPagination = ({
   getPageNumbers,
 }) => {
   return (
-    <section className="flex items-center justify-between border-t border-slate-300 w-3/4 m-auto text-slate-900 text-xs sm:text-base">
+    <section className="flex items-center justify-between border-t border-slate-300 w-3/4 m-auto text-slate-900 text-xs sm:text-sm">
       <p className="select-none">
         Page <span className="font-bold">{currentPage}</span> of {totalPages}
       </p>
       <div className="flex items-center justify-center gap-3 select-none py-12">
         {currentPage > 1 && (
           <IoIosArrowBack
-            className="w-8 h-8 sm:w-10 sm:h-10 p-1 hover:bg-slate-200 rounded cursor-pointer"
+            className="w-8 h-8 sm:w-9 sm:h-9 p-1 hover:bg-slate-200 rounded cursor-pointer"
             onClick={() => setCurrentPage(currentPage - 1)}
           />
         )}
         {getPageNumbers().map((pageNum) => (
           <ul key={pageNum}>
             <li
-              className={`px-1 sm:px-3 sm:py-1 rounded-full border cursor-pointer ${
+              className={`px-[0.3rem] sm:px-[0.35rem] lg:px-[0.6rem] lg:py-1 rounded-full border cursor-pointer ${
                 pageNum === currentPage
                   ? "bg-slate-300 font-bold border-slate-400"
                   : "hover:bg-slate-200"
@@ -34,7 +34,7 @@ const BlogPagination = ({
         ))}
         {currentPage < totalPages && (
           <IoIosArrowForward
-            className="w-8 h-8 sm:w-10 sm:h-10 p-1 hover:bg-slate-200 rounded cursor-pointer"
+            className="w-8 h-8 sm:w-9 sm:h-9 p-1 hover:bg-slate-200 rounded cursor-pointer"
             onClick={() => setCurrentPage(currentPage + 1)}
           />
         )}
